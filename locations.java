031a5d7a20524da4c6b73a48c155d72c8193bb21
gandhiniyati0127 <gandhiniyati0127@students.esuhsd.org>
2026-05-02 16:51:03 -0700

    Update locations.java

--- locations.java
+++ locations.java
@@ -1,1 +1,57 @@
+public class Locations{
+  private String name;
+  private double baseUsage;
+  private int comfort;
+  private int spiritMeter;
 
+  public Locations(String name, double baseUsage, int comfort, int spiritMeter) {
+    this.name = name;
+    this.baseUsage = baseUsage;
+    this.currentUsage = baselineUsage;
+    this.comfort = comfort;
+    this.spiritMeter = spiritMeter;
+  }
+
+  // Getters
+  public String getName(){
+    return this.name;
+  }
+
+  public double getBaseUsage(){
+    return this.baseUsage;
+  }
+  
+  public int getComfort(){
+    return this.comfort;
+  }
+  
+  public int getSpiritMeter(){
+    return this.name;
+  }
+
+  // Setters
+   public void setName(String n){
+     this.name = n;
+  }
+
+  public void serBaseUsage(double bU){
+     this.baseUsage = bU;
+  }
+  
+  public void setComfort(int c){
+     this.comfort = c;
+  }
+  
+  public void setSpiritMeter(int m){
+     this.spiritMeter = m;
+  }
+
+  public void reduceEnergy(int amount) {
+    currentUsage -= amount;
+  }
+
+  public void increaseEnergy(int amount) {
+    currentUsage += amount;
+  }
+  
+}
