39ea0c51c16077a6d0f17011db111729273c9f6d
ramakumaravibha0889 <ramakumaravibha0889@students.esuhsd.org>
2026-05-04 09:16:42 -0700

    Update LeaderBoard.java

--- LeaderBoard.java
+++ LeaderBoard.java
@@ -1,1 +1,11 @@
+import java.util.Scanner;
 
+public class LeaderBoard {
+  private int[] schools;
+
+  public LeaderBoard (String textFile){
+    File tF = new File(textFile);
+    schools = new int[7];
+    Scanner scan = new Scanner(tF);
+  }
+}
