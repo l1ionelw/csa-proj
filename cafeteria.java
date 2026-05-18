fdb20a3ee01ac2786a0e39f4871f899fa97dadfb
gandhiniyati0127 <gandhiniyati0127@students.esuhsd.org>
2026-05-18 09:32:39 -0700

    Update cafeteria.java

--- cafeteria.java
+++ cafeteria.java
@@ -1,28 +1,28 @@
-public class Cafeteria extends Zone {
+public class Classroom extends Zone {
 
-  public Cafeteria(String name, int comfort, int spiritMeter, boolean isOn) {
-    super(name, 100, comfort, spiritMeter, true);
+  public Classroom(String name, int comfort, int spiritMeter) {
+    super(name, comfort, spiritMeter);
   }
 
-  public void powerOutage() {
-    super.reduceEnergy(30);
-    super.setComfort(getComfort() - 15);
-    super.setSpiritMeter(getSpiritMeter() - 10);
+  public void openWindow() {
+    super.reduceEnergy(20);
+    super.setComfort(getComfort() + 10);
+    super.setSpiritMeter(getSpiritMeter() + 5);
   }
 
-  public void bringHomeLunch() {
-    super.reduceEnergy(15);
-    super.setComfort(getComfort() + 15);
-    super.setSpiritMeter(getSpiritMeter() + 10);
+  public void turnOnAC() {
+    super.increaseEnergy(20);
+    super.setComfort(getComfort() + 10);
+    super.setSpiritMeter(getSpiritMeter() - 5);
   }
 
-  public void cafeteriaAssembly() {
-    super.increaseEnergy(30);
-    super.setComfort(getComfort() - 10);
-    super.setSpiritMeter(getSpiritMeter() + 15);
+  public void apExams() {
+    super.increaseEnergy(20);
+    super.setComfort(getComfort() - 1);
+    super.setSpiritMeter(getSpiritMeter() - 7);
   }
 
   public String getZoneReport() {
-    return "CAFETERIA - " + super.getZoneReport();
+    return "CLASSROOM - " + super.getZoneReport();
   }
 }
