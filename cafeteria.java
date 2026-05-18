8f24d7678819306774f0b8664a95f0f9d582936c
gandhiniyati0127 <gandhiniyati0127@students.esuhsd.org>
2026-05-18 08:36:13 -0700

    Update cafeteria.java

--- cafeteria.java
+++ cafeteria.java
@@ -1,1 +1,28 @@
+public class Cafeteria extends Locations {
 
+  public Cafeteria(String name, int comfort, int spiritMeter, boolean isOn) {
+    super(name, 100, comfort, spiritMeter, true);
+  }
+
+  public void powerOutage() {
+    super.reduceEnergy(30);
+    super.setComfort(getComfort() - 15);
+    super.setSpiritMeter(getSpiritMeter() - 10);
+  }
+
+  public void bringHomeLunch() {
+    super.reduceEnergy(15);
+    super.setComfort(getComfort() + 15);
+    super.setSpiritMeter(getSpiritMeter() + 10);
+  }
+
+  public void cafeteriaAssembly() {
+    super.increaseEnergy(30);
+    super.setComfort(getComfort() - 10);
+    super.setSpiritMeter(getSpiritMeter() + 15);
+  }
+
+  public String getZoneReport() {
+    return "CAFETERIA - " + super.getZoneReport();
+  }
+}
