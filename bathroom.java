d2a985fc1f7dd8cd6314473e41688c47f3b0bccd
gandhiniyati0127 <gandhiniyati0127@students.esuhsd.org>
2026-05-18 09:33:48 -0700

    Update bathroom.java

--- bathroom.java
+++ bathroom.java
@@ -1,7 +1,10 @@
 public class Bathroom extends Zone {
 
+  private boolean isOn;
+
   public Bathroom(String name, int comfort, int spiritMeter, boolean isOn) {
-    super(name, 100, comfort, spiritMeter, true);
+    super(name, comfort, spiritMeter);
+    this.isOn = isOn;
   }
 
   public void motionSensorLights() {
@@ -14,7 +17,15 @@ public class Bathroom extends Zone {
     super.increaseEnergy(15);
     super.setComfort(getComfort() + 15);
     super.setSpiritMeter(getSpiritMeter() + 10);
-  }  
+  }
+
+  public boolean getIsOn() {
+    return isOn;
+  }
+
+  public void setIsOn(boolean isOn) {
+    this.isOn = isOn;
+  }
 
   public String getZoneReport() {
     return "BATHROOM - " + super.getZoneReport();
